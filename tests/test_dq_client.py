6fc4ab5f5bac59f678e6b3cb1327c58edc4737c6
Mateusz <mateusz.bialek@algolytics.pl>
2024-12-11 11:27:29 +0100

    Test dq

--- tests/test_dq_client.py
+++ tests/test_dq_client.py
@@ -10,21 +10,68 @@ Tests for `dq` module.
 
 import pytest
 
-
+import dq.error
 from dq import DQClient
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def test_import():
+    """Sample pytest test function.
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    import dq
+    assert 1 == 1
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.fixture(scope="session")
+def dq_client():
+    client = DQClient("https://app.dataquality.pl", "", "")
+    # with  as client:
+    yield client
+
+
+def test_client_status(dq_client):
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.account_status()
+
+
+def test_client_jobs(dq_client):
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.list_jobs()()
+
+
+def test_client_create_job(dq_client):
+    from dq import JobConfig
+    input_data = '''"ID","ADRES"
+    6876,"34-404, PYZÓWKA, PODHALAŃSKA 100"
+    '''
+    job_config = JobConfig('my job')
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.submit_job(job_config, input_data=input_data)
+
+
+def test_client_job_state(dq_client):
+    from dq import JobConfig
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.job_state('')
+
+
+def test_client_job_cancel(dq_client):
+    from dq import JobConfig
+    # with pytest.raises(dq.error.DQError, match='401'):
+    dq_client.cancel_job('')
+    assert 1
+
+
+def test_client_job_report(dq_client):
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.job_report('')
+
+
+def test_client_job_results(dq_client):
+    with pytest.raises(dq.error.DQError, match='401'):
+        dq_client.job_results('', 'output.csv')
+
+
+def test_client_job_delete(dq_client):
+    # with pytest.raises(dq.error.DQError, match='401'):
+    dq_client.delete_job('')
+    assert 1
